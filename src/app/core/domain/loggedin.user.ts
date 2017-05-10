b84fa9b32978933fbe16948ea80501d7ce51d55e
nghiepuit <nghiepuit@gmail.com>
2017-05-11 02:21:59 +0700

    update

---
+++ src/app/core/domain/loggedin.user.ts
@@ -0,0 +1,13 @@
+export class LoggedInUser{
+
+	public id : string;
+	public access_token : string;
+	public username : string;
+	public fullname : string;
+	public email : string;
+	public avatar : string; 
+
+	constructor(access_token : string, username : string, fullname : string, email: string, avatar : string){
+
+	}
+}
