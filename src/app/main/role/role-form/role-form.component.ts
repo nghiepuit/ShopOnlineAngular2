e03efd73bef77db4a4d980a1e49f8fb665c731f0
nghiepuit <nghiepuit@gmail.com>
2017-05-12 15:38:23 +0700

    like pk

--- src/app/main/role/role-form/role-form.component.ts
+++ src/app/main/role/role-form/role-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './../../../core/services/data.service';
 
 @Component({
-  selector: 'app-role-form',
-  templateUrl: './role-form.component.html',
-  styleUrls: ['./role-form.component.css']
+	selector: 'app-role-form',
+	templateUrl: './role-form.component.html',
+	styleUrls: ['./role-form.component.css']
 })
 export class RoleFormComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		private _dataService: DataService
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+
+	addRole() {
+		this._dataService.post('/api/appRoleadd');
+	}
 
 }
