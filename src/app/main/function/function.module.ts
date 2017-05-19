dcf8b70c3b517b4877a9a9c991d1df340ba90187
nghiepuit <nghiepuit@gmail.com>
2017-05-19 17:47:56 +0700

    function

--- src/app/main/function/function.module.ts
+++ src/app/main/function/function.module.ts
@@ -1,11 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+// Component
 import { FunctionComponent } from './function.component';
+// Service
+import { DataService } from './../../core/services/data.service';
+import { UploadService } from './../../core/services/upload.service';
+import { NotificationService } from './../../core/services/notification.service';
+// Shared
+
+// Third Party
+import { TreeModule } from 'angular-tree-component';
+
+const funcRoutes: Routes = [
+	{ path: '', redirectTo: 'index', pathMatch: 'full' },
+	{ path: 'index', component: FunctionComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [FunctionComponent]
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		RouterModule,
+		TreeModule,
+		RouterModule.forChild(funcRoutes)
+	],
+	declarations: [
+		FunctionComponent
+	],providers: [
+		DataService,
+		NotificationService,
+		UploadService
+	]
 })
 export class FunctionModule { }
